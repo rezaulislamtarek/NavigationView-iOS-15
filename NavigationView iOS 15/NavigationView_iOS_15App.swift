929d4b5959520d6d2ab7c766e734fbada0269d79
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-10-24 10:25:24 +0600

    Initial Commit

---
+++ NavigationView iOS 15/NavigationView_iOS_15App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationView_iOS_15App.swift
+//  NavigationView iOS 15
+//
+//  Created by Rezaul Islam on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationView_iOS_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
